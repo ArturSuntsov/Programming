5faf652766cb8f95b577763cf024e446bd129126
ArturSuntsov <ais1166611@gmail.com>
2025-11-14 21:35:18 +0500

    Update views.py

--- Practice6/Homework/DogAPI/listBreeds/views.py
+++ Practice6/Homework/DogAPI/listBreeds/views.py
@@ -1,55 +1,58 @@
 from django.shortcuts import render
 import requests
 
-# Create your views here.
+
 def list_breeds(request):
     """
-    Отображает страницу со списком всех пород собак
+    Показывает список всех пород собак
 
-    param: request: http-запрос, отправленный пользователем
+    Args:
+        request: запрос от пользователя.
 
-    return: list_breeds.html : Рендерит html страницу и передает туда список пород
+    Returns:
+        Страницу с породами (или пустой список, если что-то пошло не так).
     """
     url = 'https://dog.ceo/api/breeds/list/all'
     response = requests.get(url)
 
+    breeds = []
     if response.status_code == 200:
         data = response.json()
         breeds = list(data['message'].keys())
 
-        return render(request, 'list_breeds.html', {'breeds': breeds})
+    return render(request, 'list_breeds.html', {'breeds': breeds})
 
-    return render(request, 'list_breeds.html')
 
 def breeds_images(request):
     """
-    Отображает страницу со списком всех пород собак
+    Показывает картинки для выбранных пород.
 
-    param: request: http-запрос, отправленный пользователем
+    Args:
+        request: запрос с параметром 'breeds' (породы через запятую).
 
-    return: list_breeds.html : Рендерит html страницу для картинок собак
-            и передает туда список словарей { [порода: url-адрес картинки] }
+    Returns:
+        Страницу с картинками собак.
     """
     str_breeds = request.GET.get('breeds')
     if not str_breeds:
         return render(request, 'images_for_breeds.html', {'dogs_img': []})
 
-    list_breeds = (str_breeds.strip()).split(',')
+    list_breeds = str_breeds.strip().split(',')
+    dogs_img = []
 
-    list_urls_breeds = []
     for breed in list_breeds:
+        breed = breed.strip()
+        if not breed:
+            continue
         url = f'https://dog.ceo/api/breed/{breed}/images/random'
         response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
             if data['status'] == 'success':
-                list_urls_breeds.append(
-                    {
-                        'breed': breed.title(),
-                        'url_image': data['message']
-                    })
-            else:
-                print('Порода не найдена!')
+                dogs_img.append({
+                    'breed': breed.title(),
+                    'url_image': data['message']
+                })
 
-    return render(request, 'images_for_breeds.html', {'dogs_img': list_urls_breeds})
+    return render(request, 'images_for_breeds.html', {'dogs_img': dogs_img})
